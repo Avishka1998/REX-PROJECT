efd8308f2ea91ef823d6f823a84575bf9aa953a7
Avishka1998 <56598928+Avishka1998@users.noreply.github.com>
2020-10-02 12:01:18 +0530

    Update cust_reg.php

--- cust_reg.php
+++ cust_reg.php
@@ -10,43 +10,43 @@
     <div class="center">
       <h1>Customer Registration</h1>
 
-      <form method="post" action="#">
+      <form method="post" action="#" onsubmit="if(document.getElementById('agree').checked) { return true; } else { alert('Please indicate that you have read and agree to the Terms and Conditions and Privacy Policy'); return false; }">
         <div class="textfield">
-          <input type="text" required name="cusfirstname">
+          <input type="text" required name="first_name">
           <span></span>
           <label>First Name</label>
         </div>
 
         <div class="textfield">
-          <input type="text" required name="cuslastname">
+          <input type="text" required name="last_name">
           <span></span>
           <label>Last Name</label>
         </div>
 
         <div class="textfield">
-          <input type="email" required name="cusemail">
+          <input type="email" required name="email">
           <span></span>
           <label>Email Address</label>
         </div>
 
         <div class="textfield">
-          <input type="text" required name="cusphonenum">
+          <input type="text" required name="tele_no">
           <span></span>
           <label>Mobile Number</label>
         </div>
 
         <div class="textfield">
-          <input type="password" required name="cuspassword">
+          <input type="password" required name="password">
           <span></span>
           <label>Password</label>
         </div>
 
         <div class="textfield">
-          <input type="password" required name="cusconpassword">
+          <input type="password" required name="repeat_password">
           <span></span>
           <label>Confirm Password</label>
         </div>
-
+        <input type="checkbox" required name="checkbox" value="check" id="agree" /> I agree to the Terms and Conditions and Privacy Policy
         <input type="submit" value="Register">
       </form>
     </div>
