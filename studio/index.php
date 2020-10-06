8503f5de14d10f8121b75ddb54797ef2e4c168d3
sasindusubodhaka <58385725+sasindusubodhaka@users.noreply.github.com>
2020-10-06 09:31:39 +0530

    Add files via upload

---
+++ studio/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>RecordEx</title>
+  </head>
+  <body>
+  <?php header('Location: view/home.php'); ?>
+  </body>
+</html>
