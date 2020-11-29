6c97e2b9e52dba8c41d56360c876b59e186d87fe
asithalakshan <57120751+asithalakshan@users.noreply.github.com>
2020-11-29 15:20:07 +0530

    Pendings page and related pages craeated

---
+++ view/customer/pendings.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Pending Jobs</title>
+	<link rel="stylesheet" type="text/css" href="../../css/customer/pendings.css">
+</head>  
+<body>
+	<h2 id="finish"></h2>
+	<div class="nav body" style="padding-left: 0;">
+		<?php require_once('../../inc/cust_dash_navbar.php');?>
+	</div>
+
+	<div class="outer">
+		<div class="short-cut">
+
+
+			<div class="text">
+								
+				<a href="#finish">Finished >></a>
+				<a href="#upcoming">Up Coming >></a>
+			</div>
+		</div>
+
+		<div class="request">
+
+			<h1 id="finish">Finished</h1>
+			<div class="main-container">
+				<div class="grid">
+					<div class="pro-pic">
+						<img src="../../img/customer/profile.jpg" >
+					</div>
+			 		<div class="details">
+						<div class="name">
+							<h1>Stargate studio</h1>
+						</div>
+						<div class="mid-cont">
+							<div class="service">
+								<h2>Services</h2>
+								<p>* Recording<br>* Mixing<br>* Mastering<br>* Mixing</p>
+							</div>
+							<div class="gear">
+								<h2>Audio Gears</h2>
+								<p>* Neumann M 150 Tube<br>* Marshall DSL Series DSL40C 40 Watt Valve 2 Channel Combo</p>
+							</div>
+						</div>
+						
+						<div class="rest">
+							<h2>Date & Time</h2>
+							<p>2020/12/12<t>8.00</p>
+						</div>
+					</div>
+					<div class="buton">
+						<a href="view_finished.php" class="view" >View</a>
+						<a href="#" class="view" >Rate & Finish</a>
+						<a href="cust_complaint.php" class="complaint" >Complaint</a>
+					</div>
+				</div>
+			
+			</div>
+			<div class="main-container">
+				<div class="grid">
+					<div class="pro-pic">
+						<img src="../../img/customer/profile.jpg" >
+					</div>
+			 		<div class="details">
+						<div class="name">
+							<h1>Lowe Productions</h1>
+						</div>
+						<div class="mid-cont">
+							<div class="service">
+								<h2>Services</h2>
+								<p>* Recording<br>* Mixing<br>* Mastering<br>* Mixing<br>* Mixing</p>
+							</div>
+							<div class="gear">
+								<h2>Audio Gears</h2>
+								<p>* Neumann M 150 Tube<br>* Marshall DSL Series DSL40C 40 Watt Valve 2 Channel Combo</p>
+							</div>
+						</div>
+						
+						<div class="rest">
+							<h2>Date & Time</h2>
+							<p>2020/12/12<t>8.00</p>
+						</div>
+					</div>
+					<div class="buton">
+						<a href="view_finished.php" class="view" >View</a>
+						<a href="#" class="view" >Rate & Finish</a>
+						<a href="cust_complaint.php" class="complaint" >Complaint</a>
+					</div>
+				</div>
+			
+			</div>
+			<div class="main-container">
+				<div class="grid">
+					<div class="pro-pic">
+						<img src="../../img/customer/profile.jpg" >
+					</div>
+			 		<div class="details">
+						<div class="name">
+							<h1>Hybrid Studio</h1>
+						</div>
+						<div class="mid-cont">
+							<div class="service">
+								<h2>Services</h2>
+								<p>* Recording<br>* Mixing<br>* Mastering<br>* Mixing<br>* Mixing</p>
+							</div>
+							<div class="gear">
+								<h2>Audio Gears</h2>
+								<p>* Neumann M 150 Tube<br>* Marshall DSL Series DSL40C 40 Watt Valve 2 Channel Combo</p>
+							</div>
+						</div>
+						
+						<div class="rest">
+							<h2>Date & Time</h2>
+							<p>2020/12/12<t>8.00</p>
+						</div>
+					</div>
+					<div class="buton">
+						<a href="view_finished.php" class="view" >View</a>
+						<a href="#" class="view" >Rate & Finish</a>
+						<a href="cust_complaint.php" class="complaint" >Complaint</a>
+					</div>
+				</div>
+			
+			</div>
+			<h1 id="upcoming">Up Coming</h1>
+			<div class="main-container">
+				<div class="grid">
+					<div class="pro-pic">
+						<img src="../../img/customer/profile.jpg" >
+					</div>
+			 		<div class="details">
+						<div class="name">
+							<h1>Master Sound</h1>
+						</div>
+						<div class="mid-cont">
+							<div class="service">
+								<h2>Services</h2>
+								<p>* Recording<br>* Mixing<br>* Mastering<br>* Mixing<br>* Mixing</p>
+							</div>
+							<div class="gear">
+								<h2>Audio Gears</h2>
+								<p>* Neumann M 150 Tube<br>* Marshall DSL Series DSL40C 40 Watt Valve 2 Channel Combo</p>
+							</div>
+						</div>
+						
+						<div class="rest">
+							<h2>Date & Time</h2>
+							<p>2020/12/12<t>8.00</p>
+						</div>
+					</div>
+					<div class="buton">
+						<a href="view_ongoing.php" class="view" >View</a>
+						<a href="cust_complaint.php" class="complaint" >Complaint</a>
+					</div>
+				</div>
+			
+			</div>
+
+			<div class="main-container">
+				<div class="grid">
+					<div class="pro-pic">
+						<img src="../../img/customer/profile.jpg" >
+					</div>
+			 		<div class="details">
+	 					<div class="name">
+							<h1>Sumiga Records</h1>
+						</div>
+						<div class="mid-cont">
+							<div class="service">
+								<h2>Services</h2>
+								<p>* Recording<br>* Mixing<br>* Mastering<br>* Mixing<br>* Mixing</p>
+							</div>
+							<div class="gear">
+								<h2>Audio Gears</h2>
+								<p>* Neumann M 150 Tube<br>* Marshall DSL Series DSL40C 40 Watt Valve 2 Channel Combo</p>
+							</div>
+						</div>
+						
+						<div class="rest">
+							<h2>Date & Time</h2>
+							<p>2020/12/12<t>8.00</p>
+						</div>
+					</div>
+					<div class="buton">
+						<a href="view_ongoing.php" class="view" >View</a>
+						<a href="cust_complaint.php" class="complaint" >Complaint</a>
+					</div>
+				</div>
+			
+			</div>
+	</div>	
+	
+
+	<?php require_once('../../inc/minfooter.php'); ?>
+</body>
+</html>
---
+++ view/customer/view_ongoing.php
@@ -0,0 +1,73 @@
+
+<!DOCTYPE html>
+<html>
+<head> 
+	<meta charset="utf-8">
+	<title>Upcoming</title>
+	<link rel="stylesheet" type="text/css" href="../../css/customer/view_pending.css">
+</head>
+<body>
+	<div class="nav body" style="padding-left: 0;">
+		<?php require_once('../../inc/cust_dash_navbar.php');?>
+	</div>
+	
+	<div class="main-container">
+		<div class="grid">
+			<div class="pro-pic">
+				<img src="../../img/customer/109815470-man-avatar-profile-male-face-icon-vector-illustration-.jpg" >
+			</div>
+			<div class="details">
+				<div class="name">
+					<h1>Michel Jackson</h1>
+					<h2 style="align-content: center; padding-bottom: 0px;">jakson@gmail.com</h2>
+				</div>	
+				<div class="buton">
+
+					<a href="#" class="view" >Message</a>
+				</div>			
+			</div>			
+		</div>
+		<div class="mid-cont">
+			<div class="service">
+								
+				<ul>
+				<h2>Services</h2>
+				  <li>Recording</li>
+				  <li>Mixing</li>
+				  <li>Mastering</li>
+				  <li>Mixing</li>
+				</ul>
+			</div>
+			<div class="gear">
+				
+				<ul>
+				<h2>Audio Gears</h2>
+					<li>Neumann M 150 Tube</li>
+					<li>Marshall DSL Series DSL40C 40 Watt Valve 2 Channel Combo</li>
+				</ul>
+			</div>
+		</div>
+		<div class="rest">
+			<h2>Date & Time</h2>
+			<ul>
+				<li>2020/12/12</li>
+				<li>8.00 - 14.00</li>
+			</ul>
+		</div>
+		<div class="rest">
+			<h2>Advance Payment</h2>
+			<h3>LKR 15000/=</h3>
+		</div>
+		<div class="btn">
+			
+			<a href="cust_complaint.php" class="complaint" >Complaint</a>
+		</div>
+	</div>
+			
+		
+	
+
+
+	<?php require_once('../../inc/minfooter.php'); ?>
+</body>
+</html>
